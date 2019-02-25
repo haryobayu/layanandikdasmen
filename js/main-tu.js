8b9ec25d3c435e7921851824c0b47bd1018f715d
haryobayu <haryobayu.info@gmail.com>
2019-02-25 08:32:32 +0700

    Revisi-1
    Revisi-1

--- js/main-tu.js
+++ js/main-tu.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+$(".content-menu").hide();
+$(".tmb-open-menu").click(function(){
+  $(".content-menu").show();
+});
+$(".tmb-close-menu").click(function(){
+  $(".content-menu").hide();
+});
+
+$('.carousel').carousel({
+  interval: 6000
+})
